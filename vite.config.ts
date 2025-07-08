f36f82dc3afc82a17469ffea9859cf3a47e284c6
JulieNonnon <julie.nonnon@gmail.com>
2025-07-09 00:59:09 +0200

    fixed base ine vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'react-meteo-api', // indispensable pour le bon déploiement sur GitHub Pages
+  base: '/react-meteo-api/', // indispensable pour le bon déploiement sur GitHub Pages
 })
